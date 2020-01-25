557513f08aa8a81c21c7cfe4d4e13821ba91315c
Nathan Mentley <nathanmentley@gmail.com>
2020-01-25 18:08:30 -0500

    Adding support for displaying branches and opening a buffer listing uncovered line numbers.

--- plugin/coverlet.vim
+++ plugin/coverlet.vim
@@ -15,6 +15,9 @@ endif
 if !exists("g:coverlet_covered_color")
     let g:coverlet_covered_color="green"
 endif
+if !exists("g:coverlet_branch_color")
+    let g:coverlet_branch_color="yellow"
+endif
 
 " setup local variables
 let s:coverlet_auto_display=0
@@ -37,8 +40,9 @@ coverlet_file_name = vim.eval('g:coverlet_file_name')
 fg_color = vim.eval('g:coverlet_foreground_color')
 u_color = vim.eval('g:coverlet_uncovered_color')
 c_color = vim.eval('g:coverlet_covered_color')
+b_color = vim.eval('g:coverlet_branch_color')
 
-_coverlet = Coverlet(coverlet_file_name, fg_color, u_color, c_color)
+_coverlet = Coverlet(coverlet_file_name, fg_color, u_color, c_color, b_color)
 EOF
 
 " local vim function
@@ -56,7 +60,13 @@ endfunction
 
 " hook into vim events
 autocmd BufLeave * call s:CoverletClearIfOn()
+autocmd BufWinLeave * call s:CoverletClearIfOn()
+autocmd WinLeave * call s:CoverletClearIfOn()
+autocmd TabLeave * call s:CoverletClearIfOn()
 autocmd BufEnter * call s:CoverletRefreshIfOn()
+autocmd BufWinEnter * call s:CoverletRefreshIfOn()
+autocmd WinEnter * call s:CoverletRefreshIfOn()
+autocmd TabEnter * call s:CoverletRefreshIfOn()
 
 " Create public api global methods for the user to hit
 function! g:CoverletToggle()
@@ -73,4 +83,13 @@ function! g:CoverletRefresh()
     if s:coverlet_auto_display
         python3 _coverlet.refresh_coverlet()
     endif
+endfunction
+
+function! g:CoverletList()
+    if s:coverlet_auto_display
+        python3 _coverlet.clear_highlights()
+        let s:coverlet_auto_display = 0
+    endif
+
+    python3 _coverlet.display_coverage_info_buffer()
 endfunction
--- python/coverlet.py
+++ python/coverlet.py
@@ -22,11 +22,17 @@ class Coverlet:
     _covered_line_color: str
         *private* The currently configured background color for covered lines.
 
+    _branch_color: str
+        *private* The currently configured background color for uncovered branches.
+
     _coverlet_file: str
         *private* The currently configured file path to the coverlet json file.
 
     Methods
     -------
+
+    display_coverage_info_buffer()
+        Displays a list of uncovered lines, and branches in a new buffer and pane.
 
     clear_highlights()
         Clears the highlighting created in vim to display the coverlet data.
@@ -38,13 +44,15 @@ class Coverlet:
     _coverlet_data = {}
 
     _highlighted_lines = []
+    _highlighted_branches = []
 
     _coverlet_file = ""
     _foreground_color = ""
     _uncovered_line_color = ""
     _covered_line_color = ""
+    _branch_color = ""
 
-    def __init__(self, file_name, fg_color = "black", uncovered_color = "red", covered_color = "green"):
+    def __init__(self, file_name, fg_color = "black", uncovered_color = "red", covered_color = "green", branch_color = "yellow"):
         """
         Ctor. Sets private values from vim configuration.
         """
@@ -52,6 +60,14 @@ class Coverlet:
         self._foreground_color = fg_color
         self._uncovered_line_color = uncovered_color
         self._covered_line_color = covered_color
+        self._branch_color = branch_color
+
+    def _run_commands(self, commands):
+        """
+        runs a list of commands in vim
+        """
+        for command in commands:
+            vim.command(command)
 
     def _process_method(self, file_key, method_key, method_value):
         """
@@ -63,12 +79,21 @@ class Coverlet:
                 self._coverlet_data['files'][file_key]['uncovered_lines'].append(int(line_key))
             else:
                 self._coverlet_data['files'][file_key]['covered_lines'].append(int(line_key))
+        for branch in  method_value["Branches"]:
+            if branch["Hits"] == 0:
+                self._coverlet_data['files'][file_key]['branches'].append({
+                    "line": branch["Line"],
+                    "offset": branch["Offset"],
+                    "endOffset": branch["EndOffset"],
+                    "path": branch["Path"],
+                    "ordinal": branch["Ordinal"]
+                })
+
 
     def _load_coverlet_content(self):
         """
         Reloads the coverlet data from the json source and displays highlights in vim to visually show the data.
         """
-        self.clear_highlights()
         self._coverlet_data['files'] = {}
 
         if not os.path.exists(self._coverlet_file):
@@ -80,14 +105,14 @@ class Coverlet:
             for module_key, module_value in data.items():
                 for file_key, file_value in module_value.items():
                     self._coverlet_data['files'][file_key] = {}
+
                     self._coverlet_data['files'][file_key]['covered_lines'] = []
                     self._coverlet_data['files'][file_key]['uncovered_lines'] = []
+                    self._coverlet_data['files'][file_key]['branches'] = []
 
                     for class_key, class_value in file_value.items():
                         for method_key, method_value in class_value.items():
                             self._process_method(file_key, method_key, method_value)
-
-        self._highlight_lines()
 
     def _highlight_line(self, line, fcolor, bcolor):
         """
@@ -95,14 +120,34 @@ class Coverlet:
         """
         if not line in self._highlighted_lines:
             self._highlighted_lines.append(line)
-        vim.command("highlight CoverletLine" + str(line) + " ctermfg=" + fcolor + " ctermbg=" + bcolor)
-        vim.command('let s:coverlet_match_' + str(line) + ' = matchaddpos("CoverletLine' + str(line) + '", [0, ' + str(line) + '])')
+        self._run_commands([
+            "highlight CoverletLine{0} ctermfg={1} ctermbg={2}".format(str(line), fcolor, bcolor),
+            'let s:coverlet_match_{0} = matchaddpos("CoverletLine{1}", [{2}])'.format(str(line), str(line), str(line))
+        ])
+
+    def _highlight_branch(self, branch, fcolor, bcolor):
+        """
+        Highlights a branch in vim using the line number + offset, foreground color, and background color.
+        """
+        key = "{0}_{1}_{2}".format(branch["line"], branch["offset"], branch["endOffset"])
+        if not key in self._highlighted_branches:
+            self._highlighted_branches.append(key)
+        self._run_commands([
+            "highlight CoverletBranch{0} ctermfg={1} ctermbg={2}".format(key, fcolor, bcolor),
+            'let s:coverlet_branch_match_{0} = matchaddpos("CoverletBranch{1}", [{2}])'.format(key, key, branch["line"])
+        ])
+
+    def _clear_branch(self, key):
+        """
+        Removes a coverlet highlight in vim. This won't affect highlighting markup from other plugins.
+        """
+        vim.eval('matchdelete(s:coverlet_branch_match_{0})'.format(key))
 
     def _clear_line(self, line):
         """
         Removes a coverlet highlight in vim. This won't affect highlighting markup from other plugins.
         """
-        vim.eval('matchdelete(s:coverlet_match_' + str(line) + ')')
+        vim.eval('matchdelete(s:coverlet_match_{0})'.format(str(line)))
 
     def _get_uncovered_lines(self, file_name):
         """
@@ -122,6 +167,15 @@ class Coverlet:
         else:
             return []
 
+    def _get_branches(self, file_name):
+        """
+        Pulls a list of branch coverage.
+        """
+        if file_name in self._coverlet_data['files'].keys():
+            return self._coverlet_data['files'][file_name]['branches']
+        else:
+            return []
+
     def _highlight_lines(self):
         """
         For the current buffer. We get the full file name and try to markup the file with coverage data.
@@ -130,8 +184,34 @@ class Coverlet:
 
         for line in self._get_uncovered_lines(current_buff.name):
             self._highlight_line(line, self._foreground_color, self._uncovered_line_color)
+
         for line in self._get_covered_lines(current_buff.name):
             self._highlight_line(line, self._foreground_color, self._covered_line_color)
+
+        for branch in self._get_branches(current_buff.name):
+            self._highlight_branch(branch, self._foreground_color, self._branch_color)
+
+    def _create_new_buffer(self, file_name, contents):
+        """
+        Creates a new buffer with the passed content
+        """
+        self._run_commands([
+            'rightbelow vsplit {0}'.format(file_name),
+            'normal! ggdG',
+            'setlocal buftype=nowrite',
+            'call append(0, {0})'.format(contents)
+        ])
+
+    def display_coverage_info_buffer(self):
+        data = ["Coverage Data"]
+
+        for file_name in self._coverlet_data['files']:
+            for line in self._coverlet_data['files'][file_name]['uncovered_lines']:
+                data.append("{0}:{1} not covered.".format(file_name, line))
+            for branch in self._coverlet_data['files'][file_name]['branches']:
+                data.append("{0}:{1} branch uncovered.".format(file_name, branch["line"]))
+
+        self._create_new_buffer("Coverage Info", data)
 
     def clear_highlights(self):
         """
@@ -140,9 +220,14 @@ class Coverlet:
         for line in self._highlighted_lines:
             self._clear_line(line)
         self._highlighted_lines = []
+        for key in self._highlighted_branches:
+            self._clear_branch(key)
+        self._highlighted_branches = []
 
     def refresh_coverlet(self):
         """
         Reloads the coverlet data from the json source and displays highlights in vim to visually show the data.
         """
+        self.clear_highlights()
         self._load_coverlet_content()
+        self._highlight_lines()
